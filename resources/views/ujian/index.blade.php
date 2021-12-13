1d71431964ccf3eaf953c3e8cea294ce1e1fa40d
hendraws <saputra.hendrawijaya@gmail.com>
2021-12-13 15:25:24 +0700

    setting localstorage , view & timer

--- resources/views/ujian/index.blade.php
+++ resources/views/ujian/index.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('vendors/bootstrap-4/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/countdowntimer/jquery.countdownTimer.css') }}">
     <style>
         .bagianTitle {
             min-height: 15vh;
@@ -64,7 +65,7 @@
                                 </div>
                             </div>
                             <div class="col-md-3 align-self-center text-center">
-                                <h1>00:00</h1>
+                                <div id="countdowntimer"><span id="timer"><span></div>
                                 <a href="" class="btn btn-warning col-12">Selesai Ujian</a>
                             </div>
                         </div>
@@ -76,7 +77,8 @@
                 <div class="row p-2">
                     @for ($i = 1; $i <= count($ujian->getSoal); $i++)
                         <div class="col-md-3 col-3">
-                            <a href="javascript:void(0)" class="badge badge-secondary w-100 p-1">No.
+                            <a href="javascript:void(0)" class="badge badge-secondary w-100 p-1 nomor-urutan"
+                                data-no="{{ $i }}">No.
                                 {{ $i }}</a>
                         </div>
                     @endfor
@@ -88,11 +90,6 @@
                     <div class="row">
                         <div class="col-12">
                             @include('ujian.list_soal')
-                            <div class="col-12">
-                                <a href="" class="btn btn-info">Simpan & Lanjutkan</a>
-                                <a href="" class="btn btn-info">Ragu</a>
-                                <a href="" class="btn btn-info">Kosongkan Pilihan</a>
-                            </div>
                         </div>
                     </div>
                 </form>
@@ -106,8 +103,72 @@
     <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendors/bootstrap-4/popper.min.js') }}"></script>
     <script src="{{ asset('vendors/bootstrap-4/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('vendors/countdowntimer/jquery.countdownTimer.min.js') }}"></script>
     <script>
+        var semuaJawaban = JSON.parse(localStorage.getItem("semuaJawaban")) ?? [];
+        // console.log(semuaJawaban);
+        let urutanTerkahir = 1;
+        let urutanSekarang = 1;
+        var storedNames = JSON.parse(localStorage.getItem("semuaJawaban"));
+        let waktuBerjalan = "{{ $ujianSiswa->waktu_berjalan }}";
+        $(document).on('click', '.nomor-urutan', function() {
+            let nomorUrut = $(this).data('no');
 
+            $('#list-' + urutanTerkahir).hide();
+            $('#list-' + nomorUrut).show();
+
+            urutanTerkahir = nomorUrut;
+            urutanSekarang = nomorUrut;
+
+            console.log();
+
+        });
+
+        $('.simpan').click(function() {
+
+            var noSoal = $(this).data('soal');
+            var urutan = $(this).data('urutan');
+            var jawaban = $("input[type='radio'][name='pilihan[" + noSoal + "]']:checked").val();
+            semuaJawaban[urutan] = {
+                'soal': noSoal,
+                'jawaban': jawaban
+            };
+
+            localStorage.setItem("semuaJawaban", JSON.stringify(semuaJawaban));
+            var storedNames = JSON.parse(localStorage.getItem("semuaJawaban"));
+
+            var jumlahJawaban = storedNames.length - 1;
+            if(jumlahJawaban % 10 == 0){
+                alert(jumlahJawaban);
+            }
+            console.log($('#timer').text())
+            // console.log(storedNames, noSoal, jawaban, localStorage.getItem("semuaJawaban"), );
+
+        });
+
+
+        $.each(storedNames, function(noSoal, jawaban) {
+            if (jawaban !== null) {
+                console.log(jawaban.soal);
+                $('.myCheckbox').prop('checked', true);
+                $('#pilihan-' + jawaban.jawaban).prop("checked", true)
+            }
+        })
+
+        $(function() {
+            $("#timer").countdowntimer({
+                minutes:waktuBerjalan,
+                size: "lg",
+                borderColor : "#ffffff",
+                backgroundColor : "#ffffff",
+                fontColor : "#FA0909",
+                timeUp : timeisUp,
+            });
+        });
+
+        function timeisUp() {
+            alert('asdf');
+        }
     </script>
 
 </body>
